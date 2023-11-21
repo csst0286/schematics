7da1651cdaff5d8dba23f24cb12d4c62c0cfd397
csst0 <csst0286@yahoo.gr>
2023-11-21 16:43:44 +0100

    adding Template Files, to as schematics project, name properly the templates, conditionally templating files

--- src/order-wizard/index.ts
+++ src/order-wizard/index.ts
@@ -1,11 +1,32 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Rule, SchematicContext, Tree, apply, url, mergeWith, MergeStrategy, move, template, filter } from '@angular-devkit/schematics';
+import { normalize, strings } from '@angular-devkit/core';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function orderWizard(_options: any): Rule {
-  console.log(_options)
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    const folderPath = normalize(strings.dasherize(_options.path + '/' + _options.name));
+    let files = url('./files');
+
+    const newTree = apply(files, [
+      move(folderPath),
+      template({
+        ...strings,
+        ..._options
+      }),
+      specFilter(_options)
+    ]);
+
+    const templateRule = mergeWith(newTree, MergeStrategy.Default);
+    return templateRule(tree, _context);
   };
+}
+
+function specFilter(_options: any): Rule {
+  if (_options.spec === 'false') {
+    return filter(path => {
+      return !path.match(/\.spec\.ts$/) && !path.match(/test\.ts$/);
+    })
+  }
+  return filter(path => !path.match(/test\.ts$/));
 }
